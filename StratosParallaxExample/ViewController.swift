4810735d8ec95938d30b184e3b07952806a136f7
Claudio Carnino <carninoclaudio@gmail.com>
2014-10-18 21:17:55 +0100

    Initial Commit

---
+++ StratosParallaxExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StratosParallaxExample
+//
+//  Created by Claudio Carnino on 18/10/2014.
+//  Copyright (c) 2014 Claudio Carnino's Tugulab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
