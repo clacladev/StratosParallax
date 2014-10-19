2378cf6004071cf486c4ef5ff6c329b99cb35411
Claudio Carnino <carninoclaudio@gmail.com>
2014-10-19 11:00:33 +0100

    Fixed the size of the views to allow a seamless parallax

--- StratosParallaxExample/StratosParallaxView.swift
+++ StratosParallaxExample/StratosParallaxView.swift
@@ -20,7 +20,7 @@ class StratosParallaxView: UIView {
     
     // MARK: - Public interface
     
-    /// Array of views to animate with the parallax effect. The last view in the array will be the most foreground one
+    // Array of views to animate with the parallax effect. The last view in the array will be the most foreground one
     var viewsToAnimate: [UIView] = [] {
         willSet {
             // Remove the views added previously
@@ -34,11 +34,12 @@ class StratosParallaxView: UIView {
                 self.addSubview(viewToAnimate)
             }
             
+            fixViewsFrameForParallax()
             applyParallaxEffect()
         }
     }
     
-    /// Strenght of the parallax effect
+    // Strenght of the parallax effect
     var strength = StratosParallaxStrength.Strong
     
     
@@ -77,26 +78,63 @@ class StratosParallaxView: UIView {
     
     
     
+    /**
+    Increase the size of the views' frames, so when the parallax move the image, the views' edges are never shown
+    */
+    func fixViewsFrameForParallax() {
+        
+        let additionalSpaceNeededForParallax = CGFloat(strength.toRaw())
+        
+        for view in viewsToAnimate {
+            
+            var newFrame = view.bounds
+            newFrame.origin.x = additionalSpaceNeededForParallax / -2.0
+            newFrame.origin.y = additionalSpaceNeededForParallax / -2.0
+            newFrame.size.width += additionalSpaceNeededForParallax
+            newFrame.size.height += additionalSpaceNeededForParallax
+            
+            view.bounds = newFrame
+        }
+    }
+    
+    
+    
+    /**
+    Apply the parallax motion effect to the views to create a parallax effect.
+    Each view will have a different min/max motion effect relative value, so each view moves in a different fashion.
+    This will make the parallax effect more realistic
+    */
     func applyParallaxEffect() {
         
-        let parallaxStrengthPerView = strength.toRaw() / Double(viewsToAnimate.count)
+        // The most background view will move in the opposite direction of the most foreground one
+        let startingStrength = strength.toRaw() / -2.0
         
+        // Calculate how much changes the strength of the effect per view
+        let strengthChangePerView = strength.toRaw() / (Double(viewsToAnimate.count) - 1)
+        
+        // Apply the correct parallax strength values to the views
         for index in 0 ..< viewsToAnimate.count {
         
-            let viewToAnimate = viewsToAnimate[index]
-            let parallaxStrenghtCurrentView = parallaxStrengthPerView * Double(index + 1)
+            let strenghtCurrentView = startingStrength + strengthChangePerView * Double(index)
             
-            let horizonalInterpolation = UIInterpolatingMotionEffect(keyPath: "center.x", type: UIInterpolatingMotionEffectType.TiltAlongHorizontalAxis)
-            horizonalInterpolation.minimumRelativeValue = parallaxStrenghtCurrentView * -1
-            horizonalInterpolation.maximumRelativeValue = parallaxStrenghtCurrentView
-            
-            let verticalInterpolation = UIInterpolatingMotionEffect(keyPath: "center.y", type: UIInterpolatingMotionEffectType.TiltAlongVerticalAxis)
-            verticalInterpolation.minimumRelativeValue = parallaxStrenghtCurrentView * -1
-            verticalInterpolation.maximumRelativeValue = parallaxStrenghtCurrentView
-            
-            viewToAnimate.addMotionEffect(horizonalInterpolation)
-            viewToAnimate.addMotionEffect(verticalInterpolation)
+            applyParallaxMotionEffectToView(viewsToAnimate[index], strength: strenghtCurrentView)
         }
+    }
+    
+    
+    
+    func applyParallaxMotionEffectToView(view: UIView, strength: Double) {
+        
+        let horizonalInterpolation = UIInterpolatingMotionEffect(keyPath: "center.x", type: UIInterpolatingMotionEffectType.TiltAlongHorizontalAxis)
+        horizonalInterpolation.minimumRelativeValue = strength * -1
+        horizonalInterpolation.maximumRelativeValue = strength
+        
+        let verticalInterpolation = UIInterpolatingMotionEffect(keyPath: "center.y", type: UIInterpolatingMotionEffectType.TiltAlongVerticalAxis)
+        verticalInterpolation.minimumRelativeValue = strength * -1
+        verticalInterpolation.maximumRelativeValue = strength
+        
+        view.addMotionEffect(horizonalInterpolation)
+        view.addMotionEffect(verticalInterpolation)
     }
     
     
